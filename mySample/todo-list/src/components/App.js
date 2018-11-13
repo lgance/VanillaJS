a2abc13767d978f50aa1f73b00aae268ec0b814d
GDL <dlwodnrsms@naver.com>
2018-11-14 00:36:34 +0900

    my Todo Sample

---
+++ mySample/todo-list/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                App.js todo list 만들어보기입니다.
+            </div>
+        );
+    }
+}
+
+export default App;
