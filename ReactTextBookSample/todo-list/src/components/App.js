c9f3462d34775c89761650e264e3a577d79812b0
GDL <dlwodnrsms@naver.com>
2018-12-25 21:11:23 +0900

    main merge

--- ReactTextBookSample/todo-list/src/components/App.js
+++ ReactTextBookSample/todo-list/src/components/App.js
@@ -3,11 +3,8 @@ import PageTemplate from './PageTemplate';
 import  TodoInputContainer from '../containers/TodoInputContainer';
 import TodoListContainer from '../containers/TodoListContainer';
 
-
 class App extends Component {
- 
     render() {
-      
         return (
             <PageTemplate>
                 <TodoInputContainer/>
